24a7c17b560d2e3d29ca86080dfdc6d655272937
fayizAnj <fayiz@anjamerica.com>
2022-08-09 13:33:16 +0530

    neaws letter done

--- controllers/postMail.js
+++ controllers/postMail.js
@@ -4,6 +4,9 @@ import { contactUs } from '../utils/mailTemplate/contactUs';
 import { application } from '../utils/mailTemplate/application';
 import validator from 'validator';
 import { errors, success } from '../utils/responds';
+import subscriber from "../models/subscribers";
+import subscribers from '../models/subscribers';
+
 
 const { serverRuntimeConfig } = getConfig()
 var transporter = nodemailer.createTransport({
@@ -12,7 +15,11 @@ var transporter = nodemailer.createTransport({
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
-  }
+  },
+  pool: true,
+  maxConnections: 1,
+  rateDelta: 20000,
+  rateLimit: 5,
 });
 
 export const contactUsMailController = (req, res) => {
@@ -56,11 +63,40 @@ export const applicationMailController = (req, res) => {
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
-      return res.status(500).json(error({ status: 500, errorMessage: 'Internal Server Error', error: error.message }))
+      return res.status(500).json(errors({ status: 500, errorMessage: 'Internal Server Error', error: error.message }))
     } else {
       console.log('Email sent: ' + info.response);
       return res.status(200).json(success({ successMessage: "Email Sent Successfully", status: 200 }))
     }
   })
 }
+export const neawsLetterMail=async (req,res)=>{
+  const query=[
+    { $project : { email : 1,_id:0 }}
+  ]
+  const emails=await subscribers.aggregate(query)
+ // console.log(typeof(emails));
+  emails.forEach(email => {
+    for (let key in email) {
+        console.log(email[key]);
+        var mailOptions = {
+          from: process.env.MAIL_FROM,
+          to: email[key],
+          subject: 'Neaws Letter',
+          text: "hhhh"
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+            return res.status(500).json(errors({ status: 500, errorMessage: 'Internal Server Error', error: error.message }))
+          } else {
+            console.log('Email sent: ' + info.response);
+            return res.status(200).json(success({ successMessage: "Email Sent Successfully", status: 200 }))
+          }
+        })
+    }
+  });
+ 
+    
 
+}
--- pages/api/user/[mail].js
+++ pages/api/user/[mail].js
@@ -1,10 +1,11 @@
 
-import { applicationMailController,contactUsMailController } from "../../../controllers/postMail";
+import { applicationMailController,contactUsMailController,neawsLetterMail } from "../../../controllers/postMail";
 import API from "../../../utils/apiRoute";
 
 const apiRoute = API
 
 apiRoute.post('/api/user/applicationMail',applicationMailController )
 apiRoute.post('/api/user/contactUsMail',contactUsMailController )
+apiRoute.post('/api/user/neawsLetterMail',neawsLetterMail )
 
 export default apiRoute
