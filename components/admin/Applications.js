0819428668fbc390a59bde281cef4fdd8ab209c8
Suhail_Anj <suhail_anj@pop-os.localdomain>
2022-08-26 10:41:48 +0530

    filter button and footer icon size set ot fixed

--- components/admin/Applications.js
+++ components/admin/Applications.js
@@ -145,7 +145,7 @@ export default function Applications() {
                 onClick={() => setFilter(!filter)}
               >
                 Filter
-                <TbAdjustmentsHorizontal className="w-10 h-10" />
+                <TbAdjustmentsHorizontal className="w-5 h-5" />
               </button>
             ) : (
               <div onClick={() => router.push("/applications")}>
@@ -156,7 +156,7 @@ export default function Applications() {
                   }}
                 >
                   Close Filter
-                  <TbAdjustmentsHorizontal  className="w-10 h-10"/>
+                  <TbAdjustmentsHorizontal  className="w-5 h-5"/>
                 </button>
               </div>
             )}
