7bc5db101321182415d7d63727795d8a3d6c53df
Suhail_Anj <suhail_anj@pop-os.localdomain>
2022-09-01 16:04:58 +0530

    animation added

--- components/landin/Header.jsx
+++ components/landin/Header.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
 import { BsThreeDots } from "react-icons/bs";
 import { useState } from "react";
 import { useRouter } from "next/router";
@@ -8,242 +8,268 @@ import { useEffect } from "react";
 export default function Header() {
   const [navOpen, setNavOpen] = useState(false);
   const [active, setActive] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const logoRef = useRef(null)
+  const headRef = useRef(null);
   const router = useRouter();
   useEffect(() => {
     setActive(router.pathname);
   }, [router.pathname]);
 
+  useEffect(() => {
+    window.onscroll = function (e) {
+      let scroll = this.scrollY;
+      if (scroll >= 50) {
+        // headRef.current.style.height = "70px";
+        // logoRef.current.style.width = "130px"
+        headRef.current.style.transform="scale(0.98)";
+      } else {
+        // headRef.current.style.height = "80px";
+        // logoRef.current.style.width = "154px"
+        headRef.current.style.transform="scale(1)";
+      }
+    };
+  }, []);
+
   return (
-    <div
-      className={`${
-        router.pathname ===  ("/blogs" || "/about" || "/digitize" || "/staffing")
-          ? "relative w-full flex bg-white justify-between items-center h-[80px]  px-4  md:px-[2%] md:py-4 md:items-center md:h-[80px] sh"
-          : "relative w-full  flex justify-between items-center h-[80px]  px-4  md:px-[2%] md:py-4 md:items-center md:h-[80px] sh"
-      }`}
-
-    >
-      <div className=" xl:ml-5 w-fit">
-        <Link href="/">
-          <img
-            alt="logo image"
-            src="/landing/Anj-logo.svg"
-            className="w-fit h-[40px] md:w-[154px] ml-1 md:ml-6 object-contain cursor-pointer"
-          />
-        </Link>
-      </div>
-      <div className="hidden lg:flex justify-around gap-1 xl:gap-3 items-center md:mx-4 mr-7">
-        <Link href="/about">
-          <span
-            className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
-              active == "/about" ? "text-[#07038C]" : "text-[#08081E]"
-            }`}
-          >
-            Discover
-            <span className="w-full flex justify-center transform ease-linear duration-1000">
-              {active == "/about" && (
-                <svg
-                  width="33"
-                  height="1"
-                  viewBox="0 0 33 1"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
-                </svg>
-              )}
-            </span>
-          </span>
-        </Link>
-        <Link href="/digitize">
-          <span
-            className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
-              active == "/digitize" ? "text-[#07038C]" : "text-[#08081E]"
-            }`}
-            n
-          >
-            Digitize
-            <span className="w-full flex justify-center ease-linear duration-1000">
-              {active == "/digitize" && (
-                <svg
-                  width="33"
-                  height="1"
-                  viewBox="0 0 33 1"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
-                </svg>
-              )}
-            </span>
-          </span>
-        </Link>
-        <Link href="/staffing">
-          <span
-            className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
-              active == "/staffing" ? "text-[#07038C]" : "text-[#08081E]"
-            }`}
-          >
-            Hire Right
-            <span className="w-full flex justify-center ease-linear duration-1000">
-              {active == "/staffing" && (
-                <svg
-                  width="33"
-                  height="1"
-                  viewBox="0 0 33 1"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
-                </svg>
-              )}
-            </span>
-          </span>
-        </Link>
-        <Link href="/career">
-          <span
-            className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
-              active == "/career" ? "text-[#07038C]" : "text-[#08081E]"
-            }`}
-          >
-            Career
-            <span className="w-full flex justify-center ease-linear duration-1000">
-              {active == "/career" && (
-                <svg
-                  width="33"
-                  height="1"
-                  viewBox="0 0 33 1"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
-                </svg>
-              )}
-            </span>
-          </span>
-        </Link>
-        <Link href="/#products">
-          <span
-            className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
-              active == "/#products" ? "text-[#07038C]" : "text-[#08081E]"
-            }`}
-          >
-            Our Products
-            <span className="w-full flex justify-center ease-linear duration-1000">
-              {active == "/#products" && (
-                <svg
-                  width="33"
-                  height="1"
-                  viewBox="0 0 33 1"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
-                </svg>
-              )}
-            </span>
-          </span>
-        </Link>
-        <Link href="/blogs">
-          <span
-            className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
-              active == "/blogs" ? "text-[#07038C]" : "text-[#08081E]"
-            }`}
-          >
-            Blog
-            <span className="w-full flex justify-center ease-linear duration-1000">
-              {active == "/blogs" && (
-                <svg
-                  width="33"
-                  height="1"
-                  viewBox="0 0 33 1"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
-                </svg>
-              )}
-            </span>
-          </span>
-        </Link>
-        <Link href="/#contact">
-          <span
-            className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
-              active == "/#contact" ? "text-[#07038C]" : "text-[#08081E]"
-            }`}
-          >
-            Get in Touch
-            <span className="w-full flex justify-center ease-linear duration-1000">
-              {active == "/#contact" && (
-                <svg
-                  width="33"
-                  height="1"
-                  viewBox="0 0 33 1"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
-                </svg>
-              )}
-            </span>
-          </span>
-        </Link>
-      </div>
-      <span
-        className="h-full flex items-center justify-center lg:hidden py-4"
-        onClick={(e) => {
-          setNavOpen(!navOpen);
-        }}
+    <div>
+      <div
+        ref={headRef}
+        style={{ transition: ".3s", transform:"scale(1)", boxShadow:"none" }}
+        className={`${
+          router.pathname ===
+          ("/blogs" || "/about" || "/digitize" || "/staffing")
+            ? "relative transition-all w-full flex bg-white justify-between items-center h-[80px]  px-4  md:px-[2%] md:py-4 md:items-center md:h-[80px] sh"
+            : "relative w-full  flex justify-between items-center h-[80px]  px-4  md:px-[2%] md:py-4 md:items-center md:h-[80px] sh"
+        }`}
+        data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="1500"
       >
-        <BsThreeDots className="h-9 w-9 text-[#B4B4BE] rotate-90 self-center cursor-pointer" />
-      </span>
-      {navOpen && (
-        <div
-          className="absolute top-[4rem] right-[0rem] bg-white z-50 w-full md:hidden"
-          data-aos="fade-down"
-          data-aos-duration="1000"
-        >
-          <div className="nav-anim w-full p-3  h-fit">
-            <ul>
-              <Link href="/about">
-                <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
-                  Discover
-                </li>
-              </Link>
-              <Link href="/digitize">
-                <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
-                  Digitize
-                </li>
-              </Link>
-              <Link href="/staffing">
-                <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
-                  Hire Right
-                </li>
-              </Link>
-              <Link href="/career">
-                <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
-                  Career
-                </li>
-              </Link>
-              <Link href="/#products">
-                <li className=" text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
-                  Our Products
-                </li>
-              </Link>
-
-              <Link href="/blogs">
-                <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
-                  Blog
-                </li>
-              </Link>
-              <Link href="/#contact">
-                <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
-                  Get in Touch
-                </li>
-              </Link>
-            </ul>
-          </div>
+        <div className=" xl:ml-5 w-fit">
+          <Link href="/">
+            <img
+              ref={logoRef}
+              alt="logo image"
+              src="/landing/Anj-logo.svg"
+              className="w-fit h-[40px] md:w-[154px] ml-1 md:ml-6 object-contain cursor-pointer"
+            />
+          </Link>
         </div>
-      )}
+        <div className="hidden lg:flex justify-around gap-1 xl:gap-3 items-center md:mx-4 mr-7">
+          <Link href="/about">
+            <span
+              className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
+                active == "/about" ? "text-[#07038C]" : "text-[#08081E]"
+              }`}
+            >
+              Discover
+              <span className="w-full flex justify-center transform ease-linear duration-1000">
+                {active == "/about" && (
+                  <svg
+                    width="33"
+                    height="1"
+                    viewBox="0 0 33 1"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
+                  </svg>
+                )}
+              </span>
+            </span>
+          </Link>
+          <Link href="/digitize">
+            <span
+              className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
+                active == "/digitize" ? "text-[#07038C]" : "text-[#08081E]"
+              }`}
+              n
+            >
+              Digitize
+              <span className="w-full flex justify-center ease-linear duration-1000">
+                {active == "/digitize" && (
+                  <svg
+                    width="33"
+                    height="1"
+                    viewBox="0 0 33 1"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
+                  </svg>
+                )}
+              </span>
+            </span>
+          </Link>
+          <Link href="/staffing">
+            <span
+              className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
+                active == "/staffing" ? "text-[#07038C]" : "text-[#08081E]"
+              }`}
+            >
+              Hire Right
+              <span className="w-full flex justify-center ease-linear duration-1000">
+                {active == "/staffing" && (
+                  <svg
+                    width="33"
+                    height="1"
+                    viewBox="0 0 33 1"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
+                  </svg>
+                )}
+              </span>
+            </span>
+          </Link>
+          <Link href="/career">
+            <span
+              className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
+                active == "/career" ? "text-[#07038C]" : "text-[#08081E]"
+              }`}
+            >
+              Career
+              <span className="w-full flex justify-center ease-linear duration-1000">
+                {active == "/career" && (
+                  <svg
+                    width="33"
+                    height="1"
+                    viewBox="0 0 33 1"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
+                  </svg>
+                )}
+              </span>
+            </span>
+          </Link>
+          <Link href="/#products">
+            <span
+              className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
+                active == "/#products" ? "text-[#07038C]" : "text-[#08081E]"
+              }`}
+            >
+              Our Products
+              <span className="w-full flex justify-center ease-linear duration-1000">
+                {active == "/#products" && (
+                  <svg
+                    width="33"
+                    height="1"
+                    viewBox="0 0 33 1"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
+                  </svg>
+                )}
+              </span>
+            </span>
+          </Link>
+          <Link href="/blogs">
+            <span
+              className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
+                active == "/blogs" ? "text-[#07038C]" : "text-[#08081E]"
+              }`}
+            >
+              Blog
+              <span className="w-full flex justify-center ease-linear duration-1000">
+                {active == "/blogs" && (
+                  <svg
+                    width="33"
+                    height="1"
+                    viewBox="0 0 33 1"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
+                  </svg>
+                )}
+              </span>
+            </span>
+          </Link>
+          <Link href="/#contact">
+            <span
+              className={`cursor-pointer text-center md:text-base 2xl:text-[20px] mx-2 md:mx-4 font-semibold hover:text-primary-blue ${
+                active == "/#contact" ? "text-[#07038C]" : "text-[#08081E]"
+              }`}
+            >
+              Get in Touch
+              <span className="w-full flex justify-center ease-linear duration-1000">
+                {active == "/#contact" && (
+                  <svg
+                    width="33"
+                    height="1"
+                    viewBox="0 0 33 1"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M33 0L0 0.666667L33 1V0Z" fill="#07038C" />
+                  </svg>
+                )}
+              </span>
+            </span>
+          </Link>
+        </div>
+        <span
+          className="h-full flex items-center justify-center lg:hidden py-4"
+          onClick={(e) => {
+            setNavOpen(!navOpen);
+          }}
+        >
+          <BsThreeDots className="h-9 w-9 text-[#B4B4BE] rotate-90 self-center cursor-pointer" />
+        </span>
+        {navOpen && (
+          <div
+            className="absolute top-[4rem] right-[0rem] bg-white z-50 w-full md:hidden"
+            data-aos="fade-down"
+            data-aos-duration="1000"
+          >
+            <div className="nav-anim w-full p-3  h-fit">
+              <ul>
+                <Link href="/about">
+                  <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
+                    Discover
+                  </li>
+                </Link>
+                <Link href="/digitize">
+                  <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
+                    Digitize
+                  </li>
+                </Link>
+                <Link href="/staffing">
+                  <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
+                    Hire Right
+                  </li>
+                </Link>
+                <Link href="/career">
+                  <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
+                    Career
+                  </li>
+                </Link>
+                <Link href="/#products">
+                  <li className=" text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
+                    Our Products
+                  </li>
+                </Link>
+
+                <Link href="/blogs">
+                  <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
+                    Blog
+                  </li>
+                </Link>
+                <Link href="/#contact">
+                  <li className="text-left py-2 text-primary-blue-dark hover:text-blue-800 text-xs font-semibold">
+                    Get in Touch
+                  </li>
+                </Link>
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
