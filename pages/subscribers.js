45530ee0938717a43e974322b936bc262621db45
Suhail-k-k <suhail@anjamerica.com>
2023-06-12 11:56:45 +0530

    authlayout added for subscriber page

--- pages/subscribers.js
+++ pages/subscribers.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import Subscribers from '../components/admin/Subscribers'
+import React from "react";
+import Subscribers from "../components/admin/Subscribers";
+import AuthLayout from '../components/layout/AuthLayout';
 
 export default function subscribers() {
   return (
-    <div><Subscribers/></div>
-  )
+    <div>
+      <Subscribers />
+    </div>
+  );
 }
 
-subscribers.getLayout=AuthLayout;
+subscribers.getLayout = AuthLayout;
