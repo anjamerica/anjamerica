cdebcfda09c5f8b959104a4996b94ab471a7883f
Suhail-k-k <suhail@anjamerica.com>
2024-12-23 11:44:32 +0530

    our client moved into s3

--- components/pages/landing/VideoModal.js
+++ components/pages/landing/VideoModal.js
@@ -9,14 +9,18 @@ export default function VideoModal({ isModalVisible, handleCloseModal }) {
       onCancel={handleCloseModal}
       className="modal-custom"
     >
-      <video
+      {/* <video
         controls
         width="100%"
         className="w-fit h-[60vh]"
         src="https://nichetechies-public.s3.us-east-2.amazonaws.com/ANJ_WEBSITE/WEB/videos/h1b.mp4"
       >
         Your browser does not support the video tag.
-      </video>
+      </video> */}
+      <img
+        className="w-fit h-[70vh] object-contain"
+        src="https://nichetechies-public.s3.us-east-2.amazonaws.com/ANJ_WEBSITE/WEB/assets/posters/Brown+Modern+Illustration+United+States+(USA)+Promotion+Travel+Poster.png"
+      />
     </Modal>
   );
 }
