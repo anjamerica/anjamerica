52e3fae69e63e4efd5cb2a1a50c95384453545d5
Suhail-k-k <suhail@anjamerica.com>
2024-12-23 18:58:27 +0530

    fixes

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,13 @@ import Head from "next/head";
 import "../styles/globals.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import "antd/dist/antd.css";
 import { AuthContextProvider } from "../hooks/useAuth";
 import LoadingProvider from "../hooks/loadingContext";
 import { Toaster } from "react-hot-toast";
 import { useEffect, useState } from "react";
-import Layout from "../components/layout/Layout";
-import Header from "../components/landin/Header";
+// import Layout from "../components/layout/Layout";
+// import Header from "../components/landin/Header";
 import DefaultLayout from "../layouts/DefaultLayout";
 
 function MyApp({ Component, pageProps }) {
